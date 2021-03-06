63d855e83512c39e12505439c8974eb479a9045a
ichijiku001 <toraneko2525ry@gmail.com>
2021-03-06 16:45:35 +0900

    [update] タイトル検索機能の追加&可読性の向上

--- js/script.js
+++ js/script.js
@@ -1,75 +1,49 @@
-const sub_info_side = document.querySelector('.sub_info_side');
-const nickname = document.querySelector('.user_name > strong').textContent;
-const div = document.createElement('div');
-div.id = 'search_pixiv_menu';
+function injectHtml(path, target) {
+    fetch(chrome.runtime.getURL(path), { method: "GET" })
+    .then(response => response.text())
+    .then(text => new DOMParser().parseFromString(text, "text/html"))
+    .then(dom => dom.getElementById("_search_pixiv_menu"))
+    .then(dom => target.prepend(dom))
+    .then(() => {
+        const searchButton = document.getElementById("_searchButton");
+        const checkOption = document.getElementsByName("nick_mf");
+        const removeAtsignButton = document.getElementById("_removeAtsign");
+        const title = document.querySelector("h1.title").innerText;
+        const userName = document.querySelector('.user_name > strong').innerText;
+        formData.userInput.value = userName;
+        formData.titleInput.value = title;
+        setSearchHref();
 
-const h2 = document.createElement('h2');
-h2.textContent = 'Pixivユーザー検索';
+        formData.userInput.addEventListener("input", setSearchHref);
+        formData.titleInput.addEventListener("input", setSearchHref);
+        checkOption.forEach(element => element.addEventListener("click", changeInput));
+        removeAtsignButton.addEventListener("click", removeAtsign);
 
-const form = document.createElement('form');
-form.action = 'https://www.pixiv.net/search_user.php';
-form.method = 'get';
-form.target = '_blank';
+        function removeAtsign() {
+            if (formData.nick_mf.value !== "title") formData.userInput.value = formData.userInput.value.split(/[@＠]/)[0];
+            else if(formData.nick_mf.value === "title") formData.titleInput.value = formData.titleInput.value.split(/[@＠]/)[0];
+        }
 
-const inputText = document.createElement('input');
-inputText.type = 'text';
-inputText.className = 'search_pixiv_input';
-inputText.name = 'nick';
-inputText.value = nickname;
+        function setSearchHref() {
+            if (formData.nick_mf.value !== "title") {
+                searchButton.href = `https://www.pixiv.net/search_user.php?s_mode=s_usr&nick=${formData.userInput.value}&nick_mf=${formData.nick_mf.value}`
+            } else if (formData.nick_mf.value === "title") {
+                searchButton.href = `https://www.pixiv.net/tags/${formData.titleInput.value}/artworks?s_mode=s_tc`;
+            }
+        }
 
-const inputHidden = document.createElement('input');
-inputHidden.type = 'hidden';
-inputHidden.name = 's_mode';
-inputHidden.value = 's_usr';
-
-const inputSubmit = document.createElement('input');
-inputSubmit.type = 'submit';
-inputSubmit.value = '検索';
-
-const br = document.createElement('br');
-
-const labelRadio1 = document.createElement('label');
-
-const inputRadio1 = document.createElement('input');
-inputRadio1.type = 'radio';
-inputRadio1.name = 'nick_mf';
-inputRadio1.value = '0';
-inputRadio1.checked = 'checked';
-
-const labelRadio1Text = document.createTextNode('部分一致');
-
-const labelRadio2 = document.createElement('label');
-
-const inputRadio2 = document.createElement('input');
-inputRadio2.type = 'radio';
-inputRadio2.name = 'nick_mf';
-inputRadio2.value = '1';
-
-const labelRadio2Text = document.createTextNode('完全一致');
-
-const atSignButton = document.createElement('a');
-atSignButton.onclick = atSignRemoval;
-
-const atSignText = document.createTextNode('＠以降削除');
-
-div.appendChild(h2);
-div.appendChild(form);
-form.appendChild(inputText);
-form.appendChild(inputHidden)
-form.appendChild(inputSubmit);
-form.appendChild(br);
-form.appendChild(labelRadio1);
-labelRadio1.appendChild(inputRadio1);
-labelRadio1.appendChild(labelRadio1Text)
-form.appendChild(labelRadio2);
-labelRadio2.appendChild(inputRadio2);
-labelRadio2.appendChild(labelRadio2Text)
-form.appendChild(atSignButton);
-atSignButton.appendChild(atSignText);
-
-sub_info_side.prepend(div);
-
-function atSignRemoval(){
-    let text = document.querySelector('.search_pixiv_input');
-    text.value = text.value.split(/[@＠]/)[0];
+        function changeInput() {
+            if (formData.nick_mf.value !== "title") {
+                formData.titleInput.style.display = "none";
+                formData.userInput.style.display = "block";
+                setSearchHref();
+            } else if(formData.nick_mf.value === "title") {
+                formData.userInput.style.display = "none";
+                formData.titleInput.style.display = "block";
+                setSearchHref();
+            }
+        }
+    })    
 }
+
+injectHtml("html/searchMenu.html", document.getElementsByClassName('sub_info_side')[0]);
